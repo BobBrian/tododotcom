3ed18814e18626d4a3de7772c6aa3ebdefd5e533
BobBrian <rellrocker235@gmail.com>
2023-10-14 05:36:43 -0400

    First Commit

---
+++ components/title.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Title() {
+  return (
+    <div className="font-mono">
+        <h1 className="text-gray-50 text-6xl text-center mt-6">Chibuikem Nwacuhe</h1>
+        <h2 className="text-gray-50 text-3xl text-center mt-6">Todo List</h2>
+    </div>
+  )
+}
